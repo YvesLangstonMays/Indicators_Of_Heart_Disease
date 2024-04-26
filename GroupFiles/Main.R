8ca496f379fbb6ee61312b8bfd5957e13376330e
htnhi <hoangnhi1828@gmail.com>
2024-04-26 02:34:49 -0500

    Sampled half of the original data, add function to use as.factor() on most of the categotical variables. Performed Random Forest model on sampled data

--- GroupFiles/Main.R
+++ GroupFiles/Main.R
@@ -1,5 +1,16 @@
+install.packages("caret")
+install.packages("keras")
+install.packages("remotes")
+install.packages("tensorflow")
+
 library(ggplot2)
 library(dplyr)
+library(caret)
+library(neuralnet)
+library(randomForest)
+library(keras)
+library(tensorflow)
+library(magrittr)
 
 data <- read.csv("./GroupFiles/Indicators_Of_Heart_Disease/2022/heart_2022_no_nans.csv")
 
@@ -25,21 +36,6 @@ sum(is.na(data))
 # Transforming the data using the function
 # data <- convert_to_binary_auto(data)
 
-# Function that will look for categorical variable columns and convert them using as,factor()
-check_and_convert_categorical <- function(test_data) {
-  for (col_name in names(test_data)) {
-    if (!is.factor(test_data[[col_name]]) && (is.character(test_data[[col_name]]) || is.integer(test_data[[col_name]]))) {
-      unique_vals <- unique(test_data[[col_name]])
-      if (length(unique_vals) <= 10) {  # Adjust the threshold as needed
-        test_data[[col_name]] <- as.factor(test_data[[col_name]])
-        cat(paste("Converted", col_name, "to factor\n"))
-      }
-    }
-  }
-  return(test_data)
-}
-data = check_and_convert_categorical(data)
-summary(data)
 
 # Checking for outliers
 ggplot(data, aes(y = BMI)) +
@@ -72,3 +68,79 @@ ggplot(data, aes(y = BMI)) +
 ggplot(data, aes(y = HeightInMeters)) +
   geom_boxplot(outlier.colour = "red", outlier.shape = 1, notch = TRUE) +
   coord_flip()
+
+
+# Neural Network
+
+nn_data = data
+
+# Function that will change the variables Yes and No to numerical 1 or 0
+convert_to_binary_auto <- function(data) {
+  cols <- sapply(data, function(x) all(c("Yes", "No") %in% unique(x)))
+  cols_to_convert <- names(cols[cols == TRUE])
+
+  for (col in cols_to_convert) {
+    data[[col]] <- ifelse(data[[col]] == "Yes", 1, 0)
+  }
+
+  return(data)
+}
+# Transforming the data using the function
+nn_data <- convert_to_binary_auto(nn_data)
+
+# Performing as.factor() on other categorical variables
+check_and_convert_categorical <- function(test_data) {
+  for (col_name in names(test_data)) {
+    if (!is.factor(test_data[[col_name]]) && (is.character(test_data[[col_name]]) || is.integer(test_data[[col_name]]))) {
+      unique_vals <- unique(test_data[[col_name]])
+      if (length(unique_vals) <= 10) {  # Adjust the threshold as needed
+        test_data[[col_name]] <- as.numeric(as.factor(test_data[[col_name]]))
+      }
+    }
+  }
+  return(test_data)
+}
+nn_data = check_and_convert_categorical(nn_data)
+
+# The as.factor() function wasn't apply to the AgaCategory column since it has 13
+# levels of unique values, but we will force perform as.factor() on it anyway
+nn_data$AgeCategory = as.numeric(as.factor(nn_data$AgeCategory))
+summary(nn_data)
+
+# Find highly correlated numeric variables
+numeric_data <- nn_data[, sapply(nn_data, is.numeric)]
+corr_matrix <- cor(numeric_data)
+highly_correlated_num <- findCorrelation(corr_matrix, cutoff = 0.8)
+drop_col = colnames(numeric_data)[24]
+nn_data = nn_data[, !colnames(nn_data) %in% drop_col]
+
+summary(nn_data)
+# So we dropped the BMI column since it highly correlated to WeightInKilograms column
+
+# Split data
+n = nrow(nn_data)
+
+set.seed(100)
+train = sample(n, 0.8*n)
+
+nn_train = nn_data[train, -1]
+nn_test = nn_data[-train, -1]
+
+str(nn_train)
+
+nn_X_train = nn_train[, -which(names(nn_train) == "HadHeartAttack")]
+nn_y_train = nn_train$HadHeartAttack
+
+nn_X_test = nn_test[, -which(names(nn_test) == "HadHeartAttack")]
+nn_y_test = nn_test$HadHeartAttack
+  
+# Perform neural network on training data
+library(keras)
+
+# Define your neural network model
+model <- keras_model_sequential() %>%
+  layer_dense(units = 128, activation = 'relu', input_shape = 38) %>%
+  layer_dense(units = 64, activation = 'relu') %>%
+  layer_dense(units = 1, activation = 'sigmoid')
+
+
