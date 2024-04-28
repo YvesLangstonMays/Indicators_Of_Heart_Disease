2ef15288060e748d7ed49af2fd2349531d4af27d
YvesLangstonMays <yveslangstonmays@gmail.com>
2024-04-28 12:05:13 -0500

    Mapped Regions & Restructuring

--- GroupFiles/Main.R
+++ GroupFiles/Main.R
@@ -1,9 +1,9 @@
-install.packages("beepr")
-install.packages("ROSE")
-install.packages("caret")
-install.packages("ggplot2")
-install.packages("dplyr")
-install.packages("randomForest")
+# install.packages("beepr")
+# install.packages("ROSE")
+# install.packages("caret")
+# install.packages("ggplot2")
+# install.packages("dplyr")
+# install.packages("randomForest")
 
 library(beepr)
 library(ROSE)
@@ -11,175 +11,91 @@ library(caret)
 library(ggplot2)
 library(dplyr)
 library(randomForest)
+library(tree)
 
-data <- read.csv("./GroupFiles/Indicators_Of_Heart_Disease/2022/heart_2022_no_nans.csv")
+data <- read.csv("./Indicators_Of_Heart_Disease/2022/heart_2022_no_nans.csv")
 
 
-detach(data)
+
 attach(data)
 
-# Cleaning data
-sum(is.na(data))
-
-# There are no na values
-
-# Function that will change the variables Yes and No to numerical 1 or 0
-# convert_to_binary_auto <- function(data) {
-#   cols <- sapply(data, function(x) all(c("Yes", "No") %in% unique(x)))
-#   cols_to_convert <- names(cols[cols == TRUE])
-#   
-#   for (col in cols_to_convert) {
-#     data[[col]] <- ifelse(data[[col]] == "Yes", 1, 0)
-#   }
-#   
-#   return(data)
-# }
-# Transforming the data using the function
-# data <- convert_to_binary_auto(data)
-
-
-# Checking for outliers
-# ggplot(data, aes(y = BMI)) +
-#   geom_boxplot(outlier.colour = "red", outlier.shape = 1, notch = TRUE) +
-#   coord_flip()
-# 
-# ggplot(data, aes(y = HeightInMeters)) +
-#   geom_boxplot(outlier.colour = "red", outlier.shape = 1, notch = TRUE) +
-#   coord_flip()
-
-# Removing outliers
 data <- data %>%
-  filter(BMI <= 41)
-
-data <- data %>%
-  filter(BMI >= 14)
-
-data <- data %>%
-  filter(HeightInMeters <= 2.0)
-
-data <- data %>%
-  filter(HeightInMeters >= 1.41)
-
-data <- data %>%
-  filter(MentalHealthDays < 10)
-
-data <- data %>%
-  filter(PhysicalHealthDays <= 8)
-
-data <- data %>%
-  filter(SleepHours < 11)
-
-data <- data %>%
-  filter(SleepHours > 3)
+  filter(BMI <= 41 & BMI >= 14 &
+           HeightInMeters <= 2.0 & HeightInMeters >= 1.41 &
+           MentalHealthDays < 10 & PhysicalHealthDays <= 8 &
+           SleepHours < 11 & SleepHours > 3)
 
 
 outliers <- boxplot.stats(WeightInKilograms)$out
 data <- data %>%
   filter(!(WeightInKilograms %in% outliers))
 
-
-# # Checking Data
-# ggplot(data, aes(y = BMI)) +
-#   geom_boxplot(outlier.colour = "red", outlier.shape = 1, notch = TRUE, ) +
-#   coord_flip() 
-# 
-# # Checking Data
-# ggplot(data, aes(y = HeightInMeters)) +
-#   geom_boxplot(outlier.colour = "red", outlier.shape = 1, notch = TRUE) +
-#   coord_flip()
-
-# summary(data)
-
  data = data[, sapply(data, is.numeric)]
  dim(data)
  summary(data)
- 
- # Scaling data, explanation below
- # 
- scaled_data = scale(data)
- 
- pca_result = prcomp(scaled_data, center = TRUE, scale. = TRUE)
- summary(pca_result)
-
- loadings <- pca_result$rotation
-loadings
-pca_result$x
- # Define a threshold for "high" loading
- threshold <- 0.6
-
- # Find loading greater than the threshold
- high_loadings <- apply(loadings, 2, function(x) which(abs(x) > threshold))
-
- # Print the variable names with high loading for each principal component
- lapply(seq_along(high_loadings), function(i) {
-   cat("Principal Component", i, ":\n")
-   print(names(high_loadings[[i]]))
-   cat("\n")
- })
- plot(pca_result, type = "b")
-
-library(tree)
-set.seed(100)
-sample = sample(1:nrow(data), nrow(data) / 2)
-train_data = data[sample,]
-test_data = data[-sample,]
 
 # There is a significant difference in the number of Yes/No in our response variables
-# This may lead to much higher accuracy in predictions
-# Other measures need to be considered to evaluate the accuracy of the model
-plot(data$HadHeartAttack)
 
-
-PC4.tree <- tree(HadHeartAttack ~ PhysicalHealthDays + PhysicalActivities +
-                   HadCOPD + HadArthritis + DifficultyConcentrating + 
-                   DifficultyWalking + DifficultyDressingBathing +
-                   DifficultyErrands + ChestScan, data = train_data)
-summary(PC4.tree)
-plot(PC4.tree)
-text(PC4.tree, pretty = 0)
-
-test_predictions = predict(PC4.tree, test_data)
-test_mse = mean((test_predictions - test_data$HadHeartAttack))
-test_mse
-
-cv.PC4 = cv.tree(PC4.tree, FUN = prune.tree)
-plot(cv.PC4$size, cv.PC4$dev, type = "b")
-
-pruned = prune.tree(PC4.tree, best = 3)
-plot(pruned)
-text(pruned, pretty = 0)
+ 
+ 
+ 
+ 
+ 
+################################################################################################
+# Set seed for reproducibility 
+set.seed(4322)
 
 # We will use a sample of the data. In this case, we will only use half of the data
 num_row = nrow(data)
-set.seed(4322)
 new_data = data[sample(num_row, num_row*0.5),]
 
 # Random Forest
-
 rf_data = new_data
 
 # Performing as.factor() on other categorical variables
 check_and_convert_categorical <- function(test_data) {
   for (col_name in names(test_data)) {
-    if (!is.factor(test_data[[col_name]]) && (is.character(test_data[[col_name]]) || is.integer(test_data[[col_name]]))) {
-      unique_vals <- unique(test_data[[col_name]])
-      if (length(unique_vals) <= 10) {  # Adjust the threshold as needed
-        test_data[[col_name]] <- as.numeric(as.factor(test_data[[col_name]]))
-      }
+    if (!is.factor(test_data[[col_name]]) && (is.character(test_data[[col_name]]) || length(unique(test_data[[col_name]])) <= 10)) {
+      test_data[[col_name]] <- as.numeric(as.factor(test_data[[col_name]]))
     }
   }
   return(test_data)
 }
-rf_data = check_and_convert_categorical(rf_data)
+rf_data <- check_and_convert_categorical(new_data)
 
 # The as.factor() function wasn't apply to the AgaCategory column since it has 13
 # levels of unique values, but we will force perform as.factor() on it anyway
 rf_data$AgeCategory = as.factor(rf_data$AgeCategory)
 
-# We will drop the State columns since it will have more than 53 levels and the 
-# Random Forest won't be able to perform.
-rf_data = rf_data[, -1]
-str(rf_data)
+# Mapping states to regions
+northeast <- c("Maine", "New Hampshire", "Vermont", "Massachusetts", "Rhode Island", 
+               "Connecticut", "New York", "New Jersey", "Pennsylvania")
+midwest <- c("Ohio", "Michigan", "Indiana", "Illinois", "Wisconsin", "Minnesota", 
+             "Iowa", "Missouri", "North Dakota", "South Dakota", "Nebraska", "Kansas")
+south <- c("Delaware", "Maryland", "District of Columbia", "Virginia", "West Virginia", 
+           "Kentucky", "North Carolina", "South Carolina", "Tennessee", "Georgia", 
+           "Florida", "Alabama", "Mississippi", "Arkansas", "Louisiana", "Texas", "Oklahoma")
+west <- c("Montana", "Idaho", "Wyoming", "Colorado", "New Mexico", "Arizona", 
+          "Utah", "Nevada", "California", "Oregon", "Washington", "Alaska", "Hawaii")
+territories <- c("Puerto Rico", "Guam", "Virgin Islands")
+
+data$Region <- with(data, factor(
+  ifelse(State %in% northeast, "Northeast",
+         ifelse(State %in% midwest, "Midwest",
+                ifelse(State %in% south, "South",
+                       ifelse(State %in% west, "West", 
+                              ifelse(State %in% territories, "Territories", "Other")
+                       )
+                )
+         )
+  )
+))
+
+data <- data[, !(names(data) %in% "State")]
+
+if(any(is.na(data$Region))) {
+  warning("Some states were not categorized into any region.")
+}
 
 
 # Split data
@@ -212,7 +128,8 @@ plot(rf_model)
 # ten times in order to reduce the time.
 
 # Perform the train/test split and apply to the random forest model 10 times
-test_error_table = vector("numeric", length = 10)
+test_error_table <- numeric(10)
+
 for (i in 1:10)
 {
   set.seed(4322)
